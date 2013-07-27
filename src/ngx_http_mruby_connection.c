8f0e23ab964127010b85a416dc326c70d66360e8
cubicdaiya <cubicdaiya@gmail.com>
2013-07-28 05:16:42 +0900

    forgot to modify the information about license and copyright

--- src/ngx_http_mruby_connection.c
+++ src/ngx_http_mruby_connection.c
@@ -1,8 +1,11 @@
-/*
-// ngx_http_mruby_connection.c - ngx_mruby mruby module
-//
-// See Copyright Notice in ngx_http_mruby_module.c
-*/
+/**
+ *  Copyright (c) 2013 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+ *  Copyright (c) ngx_mruby developers 2012-
+ *
+ *  Use and distribution licensed under the MIT license.
+ *  See LICENSE for full text.
+ *
+ */
 
 #include "ngx_http_mruby_connection.h"
 #include "ngx_http_mruby_request.h"
