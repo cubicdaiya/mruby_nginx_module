cubicdaiya/mruby_nginx_module

commit 42e06caffc20854bc8e61a24f983acd67055e93e

add feature Nginx.Context

src/ngx_http_mruby_handler.c
@@ -7,6 +7,10 @@
  *
  */
 
+#include <mruby.h>
+#include <mruby/hash.h>
+#include <mruby/variable.h>
+
 #include "ngx_http_mruby_handler.h"
 #include "ngx_http_mruby_state.h"
 
@@ -22,14 +26,17 @@ ngx_int_t ngx_http_mruby_##handler_name##_handler(ngx_http_request_t *r)        
         code,                                                                                   \
         ngx_http_mruby_state_reinit_from_file                                                   \
     );                                                                                          \
-    if (ctx == NULL && (ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {                    \
-        ngx_log_error(NGX_LOG_ERR, r->connection->log                                           \
-            , 0                                                                                 \
-            , "failed to allocate memory from r->pool %s:%d"                                    \
-            , __FUNCTION__                                                                      \
-            , __LINE__                                                                          \
-        );                                                                                      \
-        return NGX_ERROR;                                                                       \
+    if (ctx == NULL) {                                                                          \
+        if ((ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {                               \
+            ngx_log_error(NGX_LOG_ERR, r->connection->log                                       \
+                , 0                                                                             \
+                , "failed to allocate memory from r->pool %s:%d"                                \
+                , __FUNCTION__                                                                  \
+                , __LINE__                                                                      \
+            );                                                                                  \
+            return NGX_ERROR;                                                                   \
+        }                                                                                       \
+        ctx->table = mrb_hash_new(mmcf->state->mrb);                                            \
     }                                                                                           \
     ctx->phase = context_phase;                                                                 \
     ngx_http_set_ctx(r, ctx, ngx_http_mruby_module);                                            \
@@ -42,14 +49,17 @@ ngx_int_t ngx_http_mruby_##handler_name##_inline_handler(ngx_http_request_t *r) 
     ngx_http_mruby_main_conf_t *mmcf = ngx_http_get_module_main_conf(r, ngx_http_mruby_module); \
     ngx_http_mruby_loc_conf_t  *mlcf = ngx_http_get_module_loc_conf(r, ngx_http_mruby_module);  \
     ngx_http_mruby_ctx_t       *ctx  = ngx_http_get_module_ctx(r, ngx_http_mruby_module);       \
-    if (ctx == NULL && (ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {                    \
-        ngx_log_error(NGX_LOG_ERR, r->connection->log                                           \
-            , 0                                                                                 \
-            , "failed to allocate memory from r->pool %s:%d"                                    \
-            , __FUNCTION__                                                                      \
-            , __LINE__                                                                          \
-        );                                                                                      \
-        return NGX_ERROR;                                                                       \
+    if (ctx == NULL) {                                                                          \
+        if ((ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {                               \
+            ngx_log_error(NGX_LOG_ERR, r->connection->log                                       \
+                , 0                                                                             \
+                , "failed to allocate memory from r->pool %s:%d"                                \
+                , __FUNCTION__                                                                  \
+                , __LINE__                                                                      \
+            );                                                                                  \
+            return NGX_ERROR;                                                                   \
+        }                                                                                       \
+        ctx->table = mrb_hash_new(mmcf->state->mrb);                                            \
     }                                                                                           \
     ctx->phase = context_phase;                                                                 \
     ngx_http_set_ctx(r, ctx, ngx_http_mruby_module);                                            \
@@ -75,16 +85,20 @@ ngx_int_t ngx_http_mruby_set_handler(ngx_http_request_t *r, ngx_str_t *val,
                                      ngx_http_variable_value_t *v, void *data)
 {
     ngx_http_mruby_loc_conf_t     *mlcf = ngx_http_get_module_loc_conf(r, ngx_http_mruby_module);
-    ngx_http_mruby_ctx_t          *ctx  = ngx_http_get_module_ctx(r, ngx_http_mruby_module); \
+    ngx_http_mruby_ctx_t          *ctx  = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
+    ngx_http_mruby_main_conf_t    *mmcf = ngx_http_get_module_main_conf(r, ngx_http_mruby_module);
     ngx_http_mruby_set_var_data_t *filter_data;
-    if (ctx == NULL && (ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log
-            , 0
-            , "failed to allocate memory from r->pool %s:%d"
-            , __FUNCTION__
-            , __LINE__
-        );
-        return NGX_ERROR;
+    if (ctx == NULL) {
+        if ((ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log
+                , 0
+                , "failed to allocate memory from r->pool %s:%d"
+                , __FUNCTION__
+                , __LINE__
+            );
+            return NGX_ERROR;
+        }
+        ctx->table = mrb_hash_new(mmcf->state->mrb);
     }
     ctx->phase = NGX_HTTP_MRUBY_PHASE_SET;
     ngx_http_set_ctx(r, ctx, ngx_http_mruby_module);
@@ -104,15 +118,19 @@ ngx_int_t ngx_http_mruby_set_inline_handler(ngx_http_request_t *r, ngx_str_t *va
                                             ngx_http_variable_value_t *v, void *data)
 {
     ngx_http_mruby_set_var_data_t *filter_data;
-    ngx_http_mruby_ctx_t          *ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module); \
-    if (ctx == NULL && (ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log
-            , 0
-            , "failed to allocate memory from r->pool %s:%d"
-            , __FUNCTION__
-            , __LINE__
-        );
-        return NGX_ERROR;
+    ngx_http_mruby_main_conf_t    *mmcf = ngx_http_get_module_main_conf(r, ngx_http_mruby_module);
+    ngx_http_mruby_ctx_t          *ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
+    if (ctx == NULL) {
+        if ((ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log
+                , 0
+                , "failed to allocate memory from r->pool %s:%d"
+                , __FUNCTION__
+                , __LINE__
+            );
+            return NGX_ERROR;
+        }
+        ctx->table = mrb_hash_new(mmcf->state->mrb);
     }
     ctx->phase = NGX_HTTP_MRUBY_PHASE_SET;
     ngx_http_set_ctx(r, ctx, ngx_http_mruby_module);

config
@@ -17,6 +17,7 @@ NGX_ADDON_SRCS="$NGX_ADDON_SRCS \
                 $ngx_addon_dir/src/ngx_http_mruby_var.c \
                 $ngx_addon_dir/src/ngx_http_mruby_connection.c \
                 $ngx_addon_dir/src/ngx_http_mruby_server.c \
+                $ngx_addon_dir/src/ngx_http_mruby_context.c \
                 "
 
 CORE_LIBS="$CORE_LIBS $mruby_root/build/host/mrblib/mrblib.o $mruby_root/build/host/lib/libmruby.a -lm"

src/ngx_http_mruby_context.c
@@ -1,0 +1,73 @@
+/**
+ *  Copyright (c) 2013 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+ *  Copyright (c) ngx_mruby developers 2012-
+ *
+ *  Use and distribution licensed under the MIT license.
+ *  See LICENSE for full text.
+ *
+ */
+
+#include "ngx_http_mruby_context.h"
+#include "ngx_http_mruby_request.h"
+
+#include <mruby.h>
+#include <mruby/proc.h>
+#include <mruby/data.h>
+#include <mruby/compile.h>
+#include <mruby/string.h>
+#include <mruby/class.h>
+
+static mrb_value ngx_mrb_get_request_context_entry(mrb_state *mrb, mrb_value self);
+static mrb_value ngx_mrb_set_request_context_entry(mrb_state *mrb, mrb_value self);
+static mrb_value ngx_mrb_get_request_context(mrb_state *mrb, mrb_value self);
+
+static mrb_value ngx_mrb_get_request_context_entry(mrb_state *mrb, mrb_value self)
+{
+    ngx_http_request_t   *r;
+    ngx_http_mruby_ctx_t *ctx;
+    mrb_value             key;
+
+    r   = ngx_mrb_get_request();
+    ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
+
+    mrb_get_args(mrb, "o", &key);
+
+    return mrb_hash_get(mrb, ctx->table, key);
+}
+
+static mrb_value ngx_mrb_set_request_context_entry(mrb_state *mrb, mrb_value self)
+{
+    ngx_http_request_t   *r;
+    ngx_http_mruby_ctx_t *ctx;
+    mrb_value             key, val;
+
+    r   = ngx_mrb_get_request();
+    ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
+
+    mrb_get_args(mrb, "oo", &key, &val);
+    mrb_hash_set(mrb, ctx->table, key, val);
+
+    return self;
+}
+
+static mrb_value ngx_mrb_get_request_context(mrb_state *mrb, mrb_value self)
+{
+    ngx_http_request_t   *r;
+    ngx_http_mruby_ctx_t *ctx;
+
+    r   = ngx_mrb_get_request();
+    ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
+
+    return ctx->table;
+}
+
+void ngx_mrb_context_class_init(mrb_state *mrb, struct RClass *class)
+{
+    struct RClass *class_context;
+
+    class_context = mrb_define_class_under(mrb, class, "Context", mrb->object_class);
+
+    mrb_define_method(mrb, class_context, "[]",    ngx_mrb_get_request_context_entry, ARGS_ANY());
+    mrb_define_method(mrb, class_context, "[]=",   ngx_mrb_set_request_context_entry, ARGS_ANY());
+    mrb_define_method(mrb, class_context, "table", ngx_mrb_get_request_context,       ARGS_ANY());
+}

src/ngx_http_mruby_context.h
@@ -7,15 +7,13 @@
  *
  */
 
-#ifndef NGX_HTTP_MRUBY_CONNECTION_H
-#define NGX_HTTP_MRUBY_CONNECTION_H
-
-#include <ngx_http.h>
+#ifndef NGX_HTTP_MRUBY_CONTEXT_H
+#define NGX_HTTP_MRUBY_CONTEXT_H
 
 #include <mruby.h>
 #include <mruby/hash.h>
 #include <mruby/variable.h>
 
-void ngx_mrb_conn_class_init(mrb_state *mrb, struct RClass *calss);
+void ngx_mrb_context_class_init(mrb_state *mrb, struct RClass *calss);
 
-#endif // NGX_HTTP_MRUBY_CONNECTION_H
+#endif // NGX_HTTP_MRUBY_CONTEXT_H

src/ngx_http_mruby_core.h
@@ -60,6 +60,7 @@ typedef struct ngx_http_mruby_ctx_t {
     ngx_mrb_rputs_chain_list_t *rputs_chain;
     ngx_http_mruby_filter_ctx_t filter_ctx;
     ngx_http_mruby_phase_t phase;
+    mrb_value table;
 } ngx_http_mruby_ctx_t;
 
 void ngx_mrb_core_init(mrb_state *mrb, struct RClass *class);

src/ngx_http_mruby_directive.c
@@ -392,7 +392,7 @@ static char *ngx_http_mruby_set_inner(ngx_conf_t *cf, ngx_command_t *cmd, void *
         return NGX_CONF_ERROR;
     }
 
-    filter_data->state = mmcf->state;
+    filter_data->state  = mmcf->state;
     filter_data->size   = filter.size;
     filter_data->script = value[2];
     if (type == NGX_MRB_CODE_TYPE_FILE) {

src/ngx_http_mruby_filter_handler.c
@@ -10,6 +10,10 @@
 #include <nginx.h>
 #include <ngx_http.h>
 
+#include <mruby.h>
+#include <mruby/hash.h>
+#include <mruby/variable.h>
+
 #include "ngx_http_mruby_module.h"
 #include "ngx_http_mruby_state.h"
 #include "ngx_http_mruby_filter_handler.h"
@@ -179,11 +183,13 @@ ngx_int_t ngx_http_mruby_body_filter_inline_handler(ngx_http_request_t *r, ngx_c
 
 static ngx_int_t ngx_http_mruby_header_filter(ngx_http_request_t *r)
 {
-    ngx_http_mruby_loc_conf_t *mlcf;
-    ngx_http_mruby_ctx_t     *ctx;
-    ngx_pool_cleanup_t *cln;
-    ngx_int_t rc;
+    ngx_http_mruby_main_conf_t *mmcf;
+    ngx_http_mruby_loc_conf_t  *mlcf;
+    ngx_http_mruby_ctx_t       *ctx;
+    ngx_pool_cleanup_t         *cln;
+    ngx_int_t                   rc;
   
+    mmcf = ngx_http_get_module_main_conf(r, ngx_http_mruby_module);
     mlcf = ngx_http_get_module_loc_conf(r, ngx_http_mruby_module);
 
     if (mlcf->header_filter_handler == NULL) {
@@ -196,9 +202,12 @@ static ngx_int_t ngx_http_mruby_header_filter(ngx_http_request_t *r)
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
 
-    if (ctx == NULL && (ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "failed to allocate memory from r->pool %s:%d", __FUNCTION__, __LINE__);
-        return NGX_ERROR;
+    if (ctx == NULL) {
+        if ((ctx = ngx_pcalloc(r->pool, sizeof(*ctx))) == NULL) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "failed to allocate memory from r->pool %s:%d", __FUNCTION__, __LINE__);
+            return NGX_ERROR;
+        }
+        ctx->table = mrb_hash_new(mmcf->state->mrb);
     }
 
     ctx->phase = NGX_HTTP_MRUBY_PHASE_HEADER_FILTER;
@@ -216,7 +225,6 @@ static ngx_int_t ngx_http_mruby_header_filter(ngx_http_request_t *r)
         return NGX_ERROR;
     }
 
-    ngx_http_mruby_main_conf_t *mmcf = ngx_http_get_module_main_conf(r, ngx_http_mruby_module);
     if (!mmcf->enabled_body_filter) {
         return ngx_http_next_header_filter(r);
     }

src/ngx_http_mruby_init.c
@@ -15,6 +15,7 @@
 #include "ngx_http_mruby_var.h"
 #include "ngx_http_mruby_connection.h"
 #include "ngx_http_mruby_server.h"
+#include "ngx_http_mruby_context.h"
 
 #include <mruby.h>
 #include <mruby/compile.h>
@@ -32,6 +33,7 @@ ngx_int_t ngx_mrb_class_init(mrb_state *mrb)
     ngx_mrb_var_class_init(mrb, class);     GC_ARENA_RESTORE;
     ngx_mrb_conn_class_init(mrb, class);    GC_ARENA_RESTORE;
     ngx_mrb_server_class_init(mrb, class);  GC_ARENA_RESTORE;
+    ngx_mrb_context_class_init(mrb, class); GC_ARENA_RESTORE;
 
     return NGX_OK;
 }

commit 811bd2248c9eb7efb64a7c3af94d0ef3fc2cd832

fix bug(regression was occurred in 42e06ca)
arena overflow error was occurred when mruby_cache is off.

src/ngx_http_mruby_handler.c
@@ -36,7 +36,13 @@ ngx_int_t ngx_http_mruby_##handler_name##_handler(ngx_http_request_t *r)        
             );                                                                                  \
             return NGX_ERROR;                                                                   \
         }                                                                                       \
+        if (!mlcf->cached) {                                                                    \
+            mmcf->state->ai = mrb_gc_arena_save(mmcf->state->mrb);                              \
+        }                                                                                       \
         ctx->table  = mrb_hash_new(mmcf->state->mrb);                                           \
+        if (!mlcf->cached) {                                                                    \
+            mrb_gc_arena_restore(mmcf->state->mrb, mmcf->state->ai);                            \
+        }                                                                                       \
         ctx->exited = 0;                                                                        \
     }                                                                                           \
     ctx->phase = context_phase;                                                                 \
@@ -60,7 +66,13 @@ ngx_int_t ngx_http_mruby_##handler_name##_inline_handler(ngx_http_request_t *r) 
             );                                                                                  \
             return NGX_ERROR;                                                                   \
         }                                                                                       \
+        if (!mlcf->cached) {                                                                    \
+            mmcf->state->ai = mrb_gc_arena_save(mmcf->state->mrb);                              \
+        }                                                                                       \
         ctx->table  = mrb_hash_new(mmcf->state->mrb);                                            \
+        if (!mlcf->cached) {                                                                    \
+            mrb_gc_arena_restore(mmcf->state->mrb, mmcf->state->ai);                            \
+        }                                                                                       \
         ctx->exited = 0;                                                                        \
     }                                                                                           \
     ctx->phase = context_phase;                                                                 \