c6ac2152999318b2abc44ddc4888b870ab970185
cubicdaiya <cubicdaiya@gmail.com>
2013-08-17 19:08:34 +0900

    modify context of mruby_cache

--- src/ngx_http_mruby_module.c
+++ src/ngx_http_mruby_module.c
@@ -38,7 +38,8 @@ static ngx_int_t ngx_http_mruby_handler_init(ngx_http_core_main_conf_t *cmcf, ng
 static ngx_command_t ngx_http_mruby_commands[] = {
 
     { ngx_string("mruby_cache"),
-      NGX_HTTP_LOC_CONF|NGX_HTTP_SRV_CONF|NGX_CONF_FLAG,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_CONF_FLAG,
       ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_mruby_loc_conf_t, cached),
