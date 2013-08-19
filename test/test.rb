9edb2822a0e2cf6d764931c740f2274d1dfc78dc
cubicdaiya <cubicdaiya@gmail.com>
2013-08-20 04:39:28 +0900

    refactoring test with yield

--- test/test.rb
+++ test/test.rb
@@ -11,92 +11,80 @@ class TestMrubyNginxModule < MiniTest::Test
       Net::EmptyPort.wait(8000, 10)
     end
 
-    def test_hello
+    def get(path)
       url = URI.parse('http://localhost:8000')
       res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/hello')
+        http.get(path)
       }
-      assert_equal(res["content-type"], "text/plain")
-      assert_equal(res.body, "Hello, World!")
+      yield res
+    end
+
+    def test_hello
+      get('/hello') { |res|
+        assert_equal(res["content-type"], "text/plain")
+        assert_equal(res.body, "Hello, World!")
+      }
     end
 
     def test_redirect
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/redirect')
+      get('/redirect') { |res|
+        assert_equal(res.code, "301")
       }
-      assert_equal(res.code, "301")
     end
 
     def test_redirect_internal
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/redirect_internal')
+      get('/redirect_internal') { |res|
+        assert_equal(res.code, "200")
+        assert_equal(res["content-type"], "text/plain")
+        assert_equal(res.body, "Hello, World!")
       }
-      assert_equal(res.code, "200")
-      assert_equal(res["content-type"], "text/plain")
-      assert_equal(res.body, "Hello, World!")
     end
 
     def test_ctx
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/ctx')
+      get('/ctx') { |res|
+        assert_equal(res.body, "3")
       }
-      assert_equal(res.body, "3")
     end
 
     def test_acl_ok
       # This test is available until 2018/08/04 for expires
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/acl.html?accesskey=bokko&expires=1533240048&signature=cDcAm6xNVlsNwuX3CP3KGg/pIPU=')
+      get('/acl.html?accesskey=bokko&expires=1533240048&signature=cDcAm6xNVlsNwuX3CP3KGg/pIPU=') { |res|
+        assert_equal(res.code, "200")
       }
-      assert_equal(res.code, "200")
     end
 
     def test_acl_ng
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/acl.html?accesskey=bokko&expires=1375558147&signature=OmZp9qEsl5GfVcxyRnIi7bGuP4I=')
+      get('/acl.html?accesskey=bokko&expires=1375558147&signature=OmZp9qEsl5GfVcxyRnIi7bGuP4I=') { |res|
+        assert_equal(res.code, "403")
       }
-      assert_equal(res.code, "403")
     end
 
     def test_header_and_body_filter
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/header_and_body_filter.html')
+      get('/header_and_body_filter.html') { |res|
+        string = IO.read("#{@dir}/ngx_base/www/header_and_body_filter.html")
+        assert_equal(res["content-type"], "text/plain")
+        assert_equal(res.body, "<!DOCTYPE html>\n" + string)
       }
-      string = IO.read("#{@dir}/ngx_base/www/header_and_body_filter.html")
-      assert_equal(res["content-type"], "text/plain")
-      assert_equal(res.body, "<!DOCTYPE html>\n" + string)
     end
 
     def test_header_filter_only
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/header_filter_only.html')
+      get('/header_filter_only.html') { |res|
+        assert_equal(res["content-type"], "text/plain")
       }
-      assert_equal(res["content-type"], "text/plain")
     end
 
     def test_body_filter_only
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/body_filter_only.html')
+      get('/body_filter_only.html') { |res|
+        string = IO.read("#{@dir}/ngx_base/www/body_filter_only.html")
+        assert_equal(res["content-type"], "text/html")
+        assert_equal(res.body, "<!DOCTYPE html>\n" + string)
       }
-      string = IO.read("#{@dir}/ngx_base/www/body_filter_only.html")
-      assert_equal(res["content-type"], "text/html")
-      assert_equal(res.body, "<!DOCTYPE html>\n" + string)
     end
 
     def test_set
-      url = URI.parse('http://localhost:8000')
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.get('/set')
+      get('/set') { |res|
+        assert_equal(res.body, "cubicdaiya\n")
       }
-      assert_equal(res.body, "cubicdaiya\n")
     end
 
     def teardown
