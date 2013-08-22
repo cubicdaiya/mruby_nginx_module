4ee845779c3e82ba2531ebac4b87d0c5bf8de819
cubicdaiya <cubicdaiya@gmail.com>
2013-08-22 23:35:09 +0900

    fix up style

--- src/ngx_http_mruby_var.c
+++ src/ngx_http_mruby_var.c
@@ -180,5 +180,5 @@ void ngx_mrb_var_class_init(mrb_state *mrb, struct RClass *class)
     class_var = mrb_define_class_under(mrb, class, "Var", mrb->object_class);
 
     mrb_define_method(mrb, class_var, "method_missing", ngx_mrb_var_method_missing, ARGS_ANY());
-    mrb_define_method(mrb, class_var, "set",            ngx_mrb_var_set, ARGS_REQ(2));
+    mrb_define_method(mrb, class_var, "set",            ngx_mrb_var_set,            ARGS_REQ(2));
 }
