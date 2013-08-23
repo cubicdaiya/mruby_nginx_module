99aca92d3cb4cc2d7a3126a443bd6389e0d4dc68
cubicdaiya <cubicdaiya@gmail.com>
2013-08-24 00:11:04 +0900

    bumped version to 0.1.1

--- src/ngx_http_mruby_module.h
+++ src/ngx_http_mruby_module.h
@@ -28,7 +28,7 @@ typedef struct {
 #endif
 
 #define MODULE_NAME        "mruby_nginx_module"
-#define MODULE_VERSION     "0.1.0"
+#define MODULE_VERSION     "0.1.1"
 
 extern ngx_module_t  ngx_http_mruby_module;
 
