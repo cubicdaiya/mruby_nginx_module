224c81bbbd825a557f77c21fdf5c643aa29ffd4c
cubicdaiya <cubicdaiya@gmail.com>
2013-08-28 02:14:31 +0900

    use https:// instead of git://

--- build_config.rb
+++ build_config.rb
@@ -15,12 +15,11 @@ MRuby::Build.new do |conf|
   conf.gembox 'default'
 
   #
-  # Recommended for mruby_nginx_module
+  # Recommended mrbgems
   #
-
-  conf.gem :git => 'git://github.com/matsumoto-r/mruby-redis.git'
-  conf.gem :git => 'git://github.com/matsumoto-r/mruby-userdata.git'
-  conf.gem :git => 'git://github.com/mattn/mruby-require.git'
+  conf.gem :git => 'https://github.com/matsumoto-r/mruby-redis.git'
+  conf.gem :git => 'https://github.com/matsumoto-r/mruby-userdata.git'
+  conf.gem :git => 'https://github.com/iij/mruby-require.git'
 
   # C compiler settings
   # conf.cc do |cc|
@@ -50,25 +49,25 @@ MRuby::Build.new do |conf|
   #   linker.option_library_path = '-L%s'
   #   linker.link_options = "%{flags} -o %{outfile} %{objs} %{libs}"
   # end
- 
+
   # Archiver settings
   # conf.archiver do |archiver|
   #   archiver.command = ENV['AR'] || 'ar'
   #   archiver.archive_options = 'rs %{outfile} %{objs}'
   # end
- 
+
   # Parser generator settings
   # conf.yacc do |yacc|
   #   yacc.command = ENV['YACC'] || 'bison'
   #   yacc.compile_options = '-o %{outfile} %{infile}'
   # end
- 
+
   # gperf settings
   # conf.gperf do |gperf|
   #   gperf.command = 'gperf'
   #   gperf.compile_options = '-L ANSI-C -C -p -j1 -i 1 -g -o -t -N mrb_reserved_word -k"1,3,$" %{infile} > %{outfile}'
   # end
-  
+
   # file extensions
   # conf.exts do |exts|
   #   exts.object = '.o'
@@ -83,12 +82,12 @@ end
 # Define cross build settings
 # MRuby::CrossBuild.new('32bit') do |conf|
 #   toolchain :gcc
-#   
+#
 #   conf.cc.flags << "-m32"
 #   conf.linker.flags << "-m32"
 #
 #   conf.build_mrbtest_lib_only
-#   
+#
 #   conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
 #
 #   conf.test_runner.command = 'env'
